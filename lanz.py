8f915a62ccc86a57216270690119e71965e4d87c
hosaka86 <64740514+hosaka86@users.noreply.github.com>
2021-05-08 03:15:40 +0200

    update lanz.py
    use zdf.de as source

--- lanz.py
+++ lanz.py
@@ -1,21 +1,20 @@
-import requests
 from bs4 import BeautifulSoup
-import time
-import unicodedata
-
-URL = 'https://www.fernsehmacher.de/markus-lanz-aktuell/'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find(id='main')
-
-fubar = []
-
-for i in results.text.split("\n"):
-    fubar.append(i)
+import urllib.request
+from gazpacho import Soup
 
 
-print(fubar[1:3])
-time.sleep(2.4)
-print(fubar[3:6])
+landing_page = urllib.request.urlopen("https://www.zdf.de/gesellschaft/markus-lanz")
+soup = BeautifulSoup(landing_page, "html.parser")
+
+link_list = []
+
+for link in soup.findAll('a'):
+    link_list.append(link.get('href'))
+
+followup = f"https://www.zdf.de{link_list[27]}" #get last from "Alle Videos der Sendung"
+
+get_guests = urllib.request.urlopen(followup)
+guest_soup = BeautifulSoup(get_guests, "html.parser")
+
+print(guest_soup.find("meta", {"property": "og:title"}).attrs['content'])
+print(guest_soup.find("meta", {"property": "og:description"}).attrs['content'])
